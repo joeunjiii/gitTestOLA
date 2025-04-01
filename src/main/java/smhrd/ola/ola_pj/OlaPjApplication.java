c1fa78e67274d0b9a5bf16330d4dd5ff65d89612
smhrd <l0_y0k0@naver.com>
2025-04-01 17:41:10 +0900

    Project 첫번째 커밋

---
+++ src/main/java/smhrd/ola/ola_pj/OlaPjApplication.java
@@ -0,0 +1,13 @@
+package smhrd.ola.ola_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlaPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/smhrd/ola/ola_pj/OlaPjApplicationTests.java
@@ -0,0 +1,13 @@
+package smhrd.ola.ola_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
