3dbef48910890c23b75bc4038e4a013572af3d7b
smhrd <l0_y0k0@naver.com>
2025-04-02 10:19:11 +0900

    OLA - 첫번째 커밋

---
+++ src/test/java/com/smhrd/olaPJ/OlaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.olaPJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
