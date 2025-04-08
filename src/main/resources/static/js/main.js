ea5b1c24831d050e7ce8919f120a4c66675d08b8
TB_POST <wl5639@naver.com>
2025-04-08 12:23:22 +0900

    박지광 main페이지 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,29 @@
+
 const navItems = document.querySelectorAll(".nav-item");
 navItems.forEach((item) => {
     item.addEventListener("click", () => {
         navItems.forEach((el) => el.classList.remove("active"));
         item.classList.add("active");
+    });
+});
+
+// 👉 슬라이드 좌우 버튼 작동
+const leftBtn = document.querySelector(".arrow.left");
+const rightBtn = document.querySelector(".arrow.right");
+const slideTrack = document.querySelector(".ott-slide-track");
+
+const scrollAmount = 300;
+
+leftBtn.addEventListener("click", () => {
+    slideTrack.scrollBy({
+        left: -scrollAmount,
+        behavior: "smooth"
+    });
+});
+
+rightBtn.addEventListener("click", () => {
+    slideTrack.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth"
     });
 });
